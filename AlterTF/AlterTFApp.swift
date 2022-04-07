3893dca2e789c1b52ada2d28937a52651c617306
张亚飞 <672233580@qq.com>
2022-04-07 10:59:47 +0800

    Initial Commit

---
+++ AlterTF/AlterTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlterTFApp.swift
+//  AlterTF
+//
+//  Created by 张亚飞 on 2022/4/7.
+//
+
+import SwiftUI
+
+@main
+struct AlterTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
