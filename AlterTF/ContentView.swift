29d70211366a0227a3398632a7c59aadd3cf7b0c
张亚飞 <672233580@qq.com>
2022-04-07 11:13:45 +0800

    alertTF

--- AlterTF/ContentView.swift
+++ AlterTF/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Button("Present Alert") {
+            
+            alertTF(title: "Login", message: "Please enter your password for @luffy", hintText: "123456", primaryTitle: "Login", secondaryTitle: "Cancle") { String in
+                
+                print(String)
+            } secondaryAction: {
+                print("Canceled")
+            }
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+extension View {
+    
+    //Mark: Alert TF
+    func alertTF(title: String, message: String, hintText:String, primaryTitle: String, secondaryTitle: String, primaryAction:@escaping(String)->(), secondaryAction: @escaping()->()) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            
+            field.placeholder = hintText
+        }
+        
+        alert.addAction(.init(title: secondaryTitle, style: .cancel, handler: { _ in
+            
+            secondaryAction()
+        }))
+        
+        alert.addAction(.init(title: primaryTitle, style: .default, handler: { _ in
+            
+            if let text = alert.textFields?[0].text {
+                
+                primaryAction(text)
+            } else {
+                
+                primaryAction("")
+            }
+        }))
+        
+        //MARK: Presenting Alert
+        rootController().present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK ROOT View Controller
+    func rootController() -> UIViewController {
+        
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            
+            return .init()
+        }
+        
+        guard let root = screen.windows.first?.rootViewController else {
+            
+            return .init()
+        }
+        
+        return root
     }
 }
